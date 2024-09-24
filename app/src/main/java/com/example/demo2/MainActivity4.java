3704d82cb46c9558946293c65e6ef71a8eff70f4
karishma pradhan <karishmapradhan9090@gmail.com>
2024-09-24 20:55:48 +0530

    24.09.20242

--- app/src/main/java/com/example/demo2/MainActivity4.java
+++ app/src/main/java/com/example/demo2/MainActivity4.java
@@ -1,5 +1,6 @@
 package com.example.demo2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,7 @@ public class MainActivity4 extends AppCompatActivity {
     private EditText editTextNumber2;
     private Button buttonAdd,next;
     private TextView textViewResult;
+    private Button joinbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +28,7 @@ public class MainActivity4 extends AppCompatActivity {
         editTextNumber2 = findViewById(R.id.secondnumber);
         buttonAdd = findViewById(R.id.submitbtn);
         textViewResult=findViewById(R.id.resultstxtview);
+        joinbutton=findViewById(R.id.joinbtn);
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -42,6 +45,12 @@ public class MainActivity4 extends AppCompatActivity {
                 int result=no1+no2;
                 textViewResult.setText("Result: " + result);
                 textViewResult.getVisibility();
+            }
+        });
+        joinbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity4.this, signup.class));
             }
         });
     }
