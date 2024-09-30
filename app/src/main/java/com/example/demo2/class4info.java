08fe77a0f9530d2a1f4a6226d9f6dc8a70b393fa
karishma pradhan <karishmapradhan9090@gmail.com>
2024-09-30 11:29:13 +0530

    30.09.202432

---
+++ app/src/main/java/com/example/demo2/class4info.java
@@ -0,0 +1,35 @@
+package com.example.demo2;
+
+public class class4info {
+
+private  String email;
+private  String password;
+
+
+
+public class4info {
+}
+
+
+public String getEmail()
+{
+
+    return email;
+}
+public void setEmail(String email) {
+
+    this.email = email;
+}
+
+public String getPassword() {
+    return password;
+}
+public void setPassword(){
+    this.password=password;
+}
+}
+
+
+
+
+
